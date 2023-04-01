e751721a1bb3c2ae569921ac5ec32539be00c506
Kacper Kwaśny <kwasny.it@gmail.com>
2023-04-01 20:29:00 +0200

    clean up serv, x, env

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='manageablereverseproxy',
     version='1.0',
-    author='Kacper Kwaśny and Mateusz Jakubowski',
+    author='Mateusz Jakubowski and Kacper Kwaśny',
     url='https://github.com/kacpekwasny/manageablereverseproxy',
     packages=find_packages(),
 )
